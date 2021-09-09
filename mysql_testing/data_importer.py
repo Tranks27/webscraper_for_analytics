3add41471df79b12842ea594b21d0df49329cf1c
Naing <tranksntb@gmail.com>
2021-09-09 11:44:38 +1000

    more data until 6th Sep

--- mysql_testing/data_importer.py
+++ mysql_testing/data_importer.py
@@ -32,18 +32,20 @@ def main():
                 print("Your are connected to database: ", record)
 
                 ## Import the data into their respective venue tables
-                for i,row in csvData.iterrows():
+                for index,row in csvData.iterrows():
                     # Check for the venue_name headers
                     if not row[0].isdigit() and not is_float(row[0]):
-                        data = csvData.iloc[i+1:i+10]
-                        print(data)
+                        data = csvData.iloc[index+1:index+10]
+                        # print(data)
                         for i,r in data.iterrows():
                             try:
                                 cursor.execute(f'INSERT INTO {row[0]} VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', tuple(r))
                             except Error as e:
+                                print(f'Got to error in executing row number {i}')
                                 print(e)
                 connection.commit()
     except Error as e:
+        print("Error in connection")
         print(e)
 
 
