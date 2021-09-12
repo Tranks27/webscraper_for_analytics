034fed35e13aa17d125e83d41e7519ed5f98464e
Naing <tranksntb@gmail.com>
2021-09-13 02:12:53 +1000

    able to select the first 9 rows from mysql server and convert back to numpy array for processing

---
+++ mysql_testing/clear_tables_query.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE central_park_bags;
+TRUNCATE TABLE crayford;
+TRUNCATE TABLE perry_barr_bags;
--- mysql_testing/test.py
+++ mysql_testing/test.py
@@ -1,25 +1,51 @@
-from getpass import getpass
+# from getpass import getpass
 from mysql.connector import connect, Error
+import pandas as pd
+import numpy as np
+
+# Read data into a variable
+# csvData = pd.read_csv('/home/tranks/scrapeBySelenium/webscraper_for_analytics/New_data.csv', delimiter=',')
 
 try:
     with connect(
         host="localhost",
-        user=input("Enter username: "),
-        password=getpass("Enter password: "),
+        # user=input("Enter username: "),
+        # password=getpass("Enter password: "),
+        user='root',
+        password='12345',
         database = "online_movie_rating"
     ) as connection:
         print(connection)
 
-        create_reviewers_table_query = """
-        CREATE TABLE reviewers (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            first_name VARCHAR(100),
-            last_name VARCHAR(100)
-        )
-        """
         with connection.cursor() as cursor:
-            cursor.execute(create_reviewers_table_query)
+            cursor.execute("SELECT database();")
+            record = cursor.fetchone()
+            print("Your are connected to database: ", record)
+            
+            selected_row = 0
+            # print("Creating table")
+            cursor.execute(f'SELECT *\
+	                        FROM testing_data_online_movie_rating\
+	                        LIMIT {selected_row},9;')
+            result = pd.DataFrame(cursor.fetchall())
+            result_np = result.to_numpy()
+            # num_rows = int(cursor.rowcount)
+            # x = map(list, list(result))
+            # x = sum(x, [])
+
+            # D = np.fromiter(iter=x, dtype=float, count=-1)
+            # D.reshape(num_rows, -1)
+
+            print(result)
+            print(result_np)
+            # for x in result:
+            #     print(x)
+            
+            lowest_odds = result_np[0][int(result_np[6][0]) - 1]
+            print(f'lowest odds: {lowest_odds}')
+
+# ROW_NUMBER() OVER()  AS row_id\
+            
             connection.commit()
 except Error as e:
     print(e)
-
