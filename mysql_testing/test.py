4ef7300e592f41382ab01fcd6aaab1ee72a3b60c
Naing <tranksntb@gmail.com>
2021-09-13 11:16:34 +1000

    strategies outlined and their matrices decided

--- mysql_testing/test.py
+++ mysql_testing/test.py
@@ -28,24 +28,31 @@ try:
 	                        FROM testing_data_online_movie_rating\
 	                        LIMIT {selected_row},9;')
             result = pd.DataFrame(cursor.fetchall())
-            result_np = result.to_numpy()
-            # num_rows = int(cursor.rowcount)
-            # x = map(list, list(result))
-            # x = sum(x, [])
+            result_np = result.to_numpy().transpose() #transpose the matrix so that each row contains each race's data
 
-            # D = np.fromiter(iter=x, dtype=float, count=-1)
-            # D.reshape(num_rows, -1)
-
-            print(result)
+            # print(result)
             print(result_np)
-            # for x in result:
-            #     print(x)
-            
-            lowest_odds = result_np[0][int(result_np[6][0]) - 1]
-            print(f'lowest odds: {lowest_odds}')
 
-# ROW_NUMBER() OVER()  AS row_id\
-            
+            ## Odds by ranking
+            rank1_odds = result_np[0][int(result_np[0][6]) - 1]
+            rank2_odds = result_np[0][int(result_np[0][7]) - 1]
+            rank3_odds = result_np[0][int(result_np[0][8]) - 1]
+            print(f'rank123_odds: {rank1_odds}, {rank2_odds}, {rank3_odds}')
+            # print(f'here: {result_np[int(result_np[8][0]) - 1][0]}')
+
+            ## Odds of the first 3 favorites 
+            lowest1_odds = sorted(result_np[0][:5])[0] #comparison between the first 6 players' odds
+            lowest2_odds = sorted(result_np[0][:5])[1]
+            lowest3_odds = sorted(result_np[0][:5])[2]
+            print(f'lowest123_odds: {lowest1_odds}, {lowest2_odds}, {lowest3_odds}')
+
+            stra1_cnts = np.zeros((6,), dtype=int)
+            stra2_cnts = np.zeros((5,6), dtype=int)
+            stra3_cnts = np.zeros((5,6), dtype=int)
+
+            print(f'Strategy 1: \n{stra1_cnts}\n')
+            print(f'Strategy 2: \n{stra2_cnts}\n')
+            print(f'Strategy 3: \n{stra3_cnts}\n')
             connection.commit()
 except Error as e:
     print(e)
