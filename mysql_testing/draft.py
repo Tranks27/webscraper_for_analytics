ac3a93a15a2afdd074bd20fc7fdfa4bac2e1bc6a
Naing <tranksntb@gmail.com>
2021-09-19 21:59:38 +1000

    sql updater working in online_movie_rating sample database

--- mysql_testing/draft.py
+++ mysql_testing/draft.py
@@ -28,31 +28,40 @@
 #################################################
 ## sql Updater
 #################################################
-with open('to_copy.txt', 'w') as f:
-    for j in range(80):
-        for i in range(6):  
-            f.write(f'Stra1_{i+1} = result[{j+1}],')
-            if i == 5:
-                break
-        break
-        
-
+# with open('to_copy.txt', 'w') as f:
+#     # for j in range(80):
+#     #     # if j < 5
+#     j = 2
+#     ## Strategy 1
 #     for i in range(6):  
-#         f.write(f'Stra2_{i+1} DECIMAL(5,2),')
-#     f.write('\\\n')
+#         f.write(f'Stra1_{i+1} = {{result[{j}]}},')
+#         j = j+1
 
-#     for i in range(6):  
-#         f.write(f'Stra3_{i+1} DECIMAL(5,2),')
-#     f.write('\\\n')
-
+#     ## Strategy 2
+#     f.write('\n')
 #     for i in range(6):
-#         for j in range(5):
-#             f.write(f'Stra4_{i+1}{j+1} DECIMAL(5,2),')
-#     f.write('\\\n')
+#         f.write(f'Stra2_{i+1} = {{result[{j}]}},')
+#         j = j+1
 
+#     ## Strategy 3
+#     f.write('\n')
 #     for i in range(6):
-#         for j in range(5):
-#             f.write(f'Stra5_{i+1}{j+1} DECIMAL(5,2),')
+#         f.write(f'Stra3_{i+1} = {{result[{j}]}},')
+#         j = j+1
+
+#     ## Strategy 4
+#     f.write('\n')
+#     for k in range(6):
+#         for i in range(5):
+#             f.write(f'Stra4_{k+1}{i+1} = {{result[{j}]}},')
+#             j = j+1
+            
+#     ## Strategy 5
+#     f.write('\n')
+#     for k in range(6):
+#         for i in range(5):
+#             f.write(f'Stra5_{k+1}{i+1} = {{result[{j}]}},')
+#             j = j+1
 
 
 
