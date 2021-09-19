averaging bug fixed. omrDB functional now

mysql_testing/omrdb_venue_results_analyzer.py
@@ -7,16 +7,15 @@ import csv
 
 ##TODO: Choose type of operation
 opr_mode = 1 # 0 = INSERT(used only for the first time); 1 = SELECT & UPDATE existing data
-venue_id = 3
-venue_name = 'test_data_two'
-
+venue_id = 1 # opr_mode 0: venue_id for new venue, opr_mode 1: venue_id that we want to update. NOTE: venue_id is only defined in strategies table, not in the actual venue table, hence the necessity of inputing both venue_id & venue_name
+venue_name = 'test_data_two' # name of venue table that we want to extract new data form
+percentages_table = "strategies_results_v2"
 ###########################################
 ## Upload to mysql server
 ###########################################
 def sql_uploader(new_data):
     
     print(f'new_data: size = {len(new_data)}\n{new_data}')
-    table_name = "strategies_results"
     try:
         with connect(
             host="localhost",
@@ -35,8 +34,9 @@ def sql_uploader(new_data):
                 if opr_mode == 0:
                     try:
                         # use INSERT if first time adding results for a venue
-                        cursor.execute(f'INSERT INTO {table_name}\
-                                            VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
+                        cursor.execute(f'INSERT INTO {percentages_table}\
+                                            VALUES(%s,\
+                                            %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
                                             %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
                                             %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
                                             %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
@@ -52,7 +52,7 @@ def sql_uploader(new_data):
                     try:
                         ## read the existing data
                         cursor.execute(f'SELECT *\
-                                        FROM {table_name}\
+                                        FROM {percentages_table}\
                                         WHERE venue_id = {venue_id}')
                         old_data = pd.DataFrame(cursor.fetchall()).to_numpy(dtype=float)
                         old_data = old_data[0] # get rid of double brackets and convert to one bracket only
@@ -66,15 +66,16 @@ def sql_uploader(new_data):
 
                     ## Update the data
                     updated_data = np.add(old_data, new_data)
+                    updated_data[3:] = np.divide(updated_data[3:],2) # divide the percentages by 2 to get the average, otherwise % will exceed 100% overtime
                     print(f'updated_data: size = {len(updated_data)}\n{updated_data}')
                     try:
-                        cursor.execute(f'UPDATE {table_name}\
-                                        SET num_races = {updated_data[1]},\
-                                        Stra1_1 = {updated_data[2]},Stra1_2 = {updated_data[3]},Stra1_3 = {updated_data[4]},Stra1_4 = {updated_data[5]},Stra1_5 = {updated_data[6]},Stra1_6 = {updated_data[7]},\
-                                        Stra2_1 = {updated_data[8]},Stra2_2 = {updated_data[9]},Stra2_3 = {updated_data[10]},Stra2_4 = {updated_data[11]},Stra2_5 = {updated_data[12]},Stra2_6 = {updated_data[13]},\
-                                        Stra3_1 = {updated_data[14]},Stra3_2 = {updated_data[15]},Stra3_3 = {updated_data[16]},Stra3_4 = {updated_data[17]},Stra3_5 = {updated_data[18]},Stra3_6 = {updated_data[19]},\
-                                        Stra4_11 = {updated_data[20]},Stra4_12 = {updated_data[21]},Stra4_13 = {updated_data[22]},Stra4_14 = {updated_data[23]},Stra4_15 = {updated_data[24]},Stra4_21 = {updated_data[25]},Stra4_22 = {updated_data[26]},Stra4_23 = {updated_data[27]},Stra4_24 = {updated_data[28]},Stra4_25 = {updated_data[29]},Stra4_31 = {updated_data[30]},Stra4_32 = {updated_data[31]},Stra4_33 = {updated_data[32]},Stra4_34 = {updated_data[33]},Stra4_35 = {updated_data[34]},Stra4_41 = {updated_data[35]},Stra4_42 = {updated_data[36]},Stra4_43 = {updated_data[37]},Stra4_44 = {updated_data[38]},Stra4_45 = {updated_data[39]},Stra4_51 = {updated_data[40]},Stra4_52 = {updated_data[41]},Stra4_53 = {updated_data[42]},Stra4_54 = {updated_data[43]},Stra4_55 = {updated_data[44]},Stra4_61 = {updated_data[45]},Stra4_62 = {updated_data[46]},Stra4_63 = {updated_data[47]},Stra4_64 = {updated_data[48]},Stra4_65 = {updated_data[49]},\
-                                        Stra5_11 = {updated_data[50]},Stra5_12 = {updated_data[51]},Stra5_13 = {updated_data[52]},Stra5_14 = {updated_data[53]},Stra5_15 = {updated_data[54]},Stra5_21 = {updated_data[55]},Stra5_22 = {updated_data[56]},Stra5_23 = {updated_data[57]},Stra5_24 = {updated_data[58]},Stra5_25 = {updated_data[59]},Stra5_31 = {updated_data[60]},Stra5_32 = {updated_data[61]},Stra5_33 = {updated_data[62]},Stra5_34 = {updated_data[63]},Stra5_35 = {updated_data[64]},Stra5_41 = {updated_data[65]},Stra5_42 = {updated_data[66]},Stra5_43 = {updated_data[67]},Stra5_44 = {updated_data[68]},Stra5_45 = {updated_data[69]},Stra5_51 = {updated_data[70]},Stra5_52 = {updated_data[71]},Stra5_53 = {updated_data[72]},Stra5_54 = {updated_data[73]},Stra5_55 = {updated_data[74]},Stra5_61 = {updated_data[75]},Stra5_62 = {updated_data[76]},Stra5_63 = {updated_data[77]},Stra5_64 = {updated_data[78]},Stra5_65 = {updated_data[79]}\
+                        cursor.execute(f'UPDATE {percentages_table}\
+                                        SET num_races = {updated_data[1]}, num_days = {updated_data[2]},\
+                                        Stra1_1 = {updated_data[3]},Stra1_2 = {updated_data[4]},Stra1_3 = {updated_data[5]},Stra1_4 = {updated_data[6]},Stra1_5 = {updated_data[7]},Stra1_6 = {updated_data[8]},\
+                                        Stra2_1 = {updated_data[9]},Stra2_2 = {updated_data[10]},Stra2_3 = {updated_data[11]},Stra2_4 = {updated_data[12]},Stra2_5 = {updated_data[13]},Stra2_6 = {updated_data[14]},\
+                                        Stra3_1 = {updated_data[15]},Stra3_2 = {updated_data[16]},Stra3_3 = {updated_data[17]},Stra3_4 = {updated_data[18]},Stra3_5 = {updated_data[19]},Stra3_6 = {updated_data[20]},\
+                                        Stra4_11 = {updated_data[21]},Stra4_12 = {updated_data[22]},Stra4_13 = {updated_data[23]},Stra4_14 = {updated_data[24]},Stra4_15 = {updated_data[25]},Stra4_21 = {updated_data[26]},Stra4_22 = {updated_data[27]},Stra4_23 = {updated_data[28]},Stra4_24 = {updated_data[29]},Stra4_25 = {updated_data[30]},Stra4_31 = {updated_data[31]},Stra4_32 = {updated_data[32]},Stra4_33 = {updated_data[33]},Stra4_34 = {updated_data[34]},Stra4_35 = {updated_data[35]},Stra4_41 = {updated_data[36]},Stra4_42 = {updated_data[37]},Stra4_43 = {updated_data[38]},Stra4_44 = {updated_data[39]},Stra4_45 = {updated_data[40]},Stra4_51 = {updated_data[41]},Stra4_52 = {updated_data[42]},Stra4_53 = {updated_data[43]},Stra4_54 = {updated_data[44]},Stra4_55 = {updated_data[45]},Stra4_61 = {updated_data[46]},Stra4_62 = {updated_data[47]},Stra4_63 = {updated_data[48]},Stra4_64 = {updated_data[49]},Stra4_65 = {updated_data[50]},\
+                                        Stra5_11 = {updated_data[51]},Stra5_12 = {updated_data[52]},Stra5_13 = {updated_data[53]},Stra5_14 = {updated_data[54]},Stra5_15 = {updated_data[55]},Stra5_21 = {updated_data[56]},Stra5_22 = {updated_data[57]},Stra5_23 = {updated_data[58]},Stra5_24 = {updated_data[59]},Stra5_25 = {updated_data[60]},Stra5_31 = {updated_data[61]},Stra5_32 = {updated_data[62]},Stra5_33 = {updated_data[63]},Stra5_34 = {updated_data[64]},Stra5_35 = {updated_data[65]},Stra5_41 = {updated_data[66]},Stra5_42 = {updated_data[67]},Stra5_43 = {updated_data[68]},Stra5_44 = {updated_data[69]},Stra5_45 = {updated_data[70]},Stra5_51 = {updated_data[71]},Stra5_52 = {updated_data[72]},Stra5_53 = {updated_data[73]},Stra5_54 = {updated_data[74]},Stra5_55 = {updated_data[75]},Stra5_61 = {updated_data[76]},Stra5_62 = {updated_data[77]},Stra5_63 = {updated_data[78]},Stra5_64 = {updated_data[79]},Stra5_65 = {updated_data[80]}\
                                         WHERE venue_id = {venue_id}')
 
                     except Error as e:
@@ -113,16 +114,17 @@ def main():
                 ## Apply strategies on each venue, each day 
                 ###########################################
                 sum_arr = np.zeros((80,), dtype=int)
+                days_to_add = 0
                 ## Count number of entries in the venue table
                 cursor.execute(f'SELECT COUNT(*)\
                                 FROM {venue_name};')
                 entries_cnt = int(pd.DataFrame(cursor.fetchone()).to_numpy())
                 # print(f'row count = {entries_cnt}')
-
+                
                 ## Start the operation of calculating strategy results 
                 for starting_row in range(0,entries_cnt-1,9):
                     # print(f'starting_row = {starting_row}')
-                    
+                    days_to_add = days_to_add + 1
                     ## select the 9 rows block
                     cursor.execute(f'SELECT *\
                                     FROM {venue_name}\
@@ -395,11 +397,11 @@ def main():
                 num_races = sum_arr[1]
                 sum_arr = np.delete(sum_arr,[0,1]) # delete the venue_id and num_races from the array
                 ## convert into percentages and create the array to be uploaded to mysql
-                new_data = np.array([venue_id, num_races])
+                new_data = np.array([venue_id, num_races, days_to_add])
                 for i in range(len(sum_arr)):
                     new_data = np.append(new_data, round(float(sum_arr[i]/num_races)*100, 2))
                 # print(f'new_data: size = {len(new_data)}\n{new_data}')
-
+                
                 connection.commit()
                 print(f'main db_connection COMMITTED!!!..........\n')
     except Error as e: